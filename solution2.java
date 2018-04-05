44395cc812419528837619954a6c9a8d162900f6
shakshi-12 <38070982+shakshi-12@users.noreply.github.com>
2018-04-05 19:43:30 +0530

    Add files via upload

---
+++ solution2.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    String str="";
+		Scanner sc= new Scanner(System.in);
+		int l= sc.nextInt();
+		int arr[]= new int[l];
+		String arr1[]= new String[];
+		for(int i=0;i<l;i++)
+		{
+		    String str = sc.nextLine();
+		    arr[i]= str.indexOf();
+		}
+		for(int j=0;j<l;j++)
+		{
+		    value= arr[j];
+		    for(int k=j+1;k<l;k++)
+		    {
+		        if(arr[k]<value)
+		        {
+		            value= arr[k];
+		            arr
+		        }
+		        s=
+		    }
+		    str= str+value;
+		    
+		}
+		
+		
+	}
+}
